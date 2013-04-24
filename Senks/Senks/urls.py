cc96170aad035584a772977cbf37fc0f48687287
Brian Chano <brio.ochan@gmail.com>
2013-04-24 13:32:17 +0300

    Mapped time url in url.py

--- Senks/Senks/urls.py
+++ Senks/Senks/urls.py
@@ -1,13 +1,16 @@
 from django.conf.urls import patterns, include, url
+from mysite.views import current_datetime #, hours_ahead
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
+	(r'^time/$', current_datetime),
+	#(r'^time/plus/(\d{1,2})/$', hours_ahead),
     # Examples:
-    # url(r'^$', 'Senks.views.home', name='home'),
-    # url(r'^Senks/', include('Senks.foo.urls')),
+    # url(r'^$', 'mysite.views.home', name='home'),
+    # url(r'^mysite/', include('mysite.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
@@ -15,3 +18,4 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
+
